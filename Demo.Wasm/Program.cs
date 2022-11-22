e12c195e07f714589844c9df32cc39586db34710
Alex Chow <zhouchuanglin@gmail.com>
2022-11-22 19:05:08 +0100

    up Demo.Wasm to net7.0

--- Demo.Wasm/Program.cs
+++ Demo.Wasm/Program.cs
@@ -1,10 +1,11 @@
-﻿using Demo.Wasm;
+using Demo.Wasm;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("app");
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
-
